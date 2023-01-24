79f7d4b39784a009dac9e289b5dc18ef46131f32
Andy Lam <andy.h.lam1@gmail.com>
2023-01-23 16:10:42 -0800

    Manufacturer list and form and vehicle model list done

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,56 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import React, {useEffect, useState} from 'react'
+import ManufacturerList from './ListManufacturers';
+import ManufacturerForm from './ManufacturerForm';
+import VehicleModelList from './ListVehicleModels';
+
+
 
 function App() {
+// Fetching all the data
+  const [manufacturers, setManufacturers] = useState([]);
+  const fetchManufacturers = async () => {
+    const url = 'http://localhost:8100/api/manufacturers/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setManufacturers(data.manufacturers)
+      }
+  }
+
+  const [models, setModels] = useState([]);
+  const fetchModels = async () => {
+    const url = 'http://localhost:8100/api/models/';
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setModels(data.models)
+      }
+  }
+
+
+// Using the effects
+  useEffect(() => {
+    fetchManufacturers();
+    fetchModels();
+  }, []);
+
+////// OUR JSX
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturers" >
+            <Route path="" element={<ManufacturerList manufacturers={manufacturers} />} />
+            <Route path="new" element={<ManufacturerForm/>} fetchManufacturers={fetchManufacturers}/>
+          </Route>
+          <Route path="models" >
+            <Route path="" element={<VehicleModelList models={models}/>} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,6 +10,26 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/manufacturers">
+                Manufacturers List
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/manufacturers/new">
+                Manufacturers Form
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/models">
+                Vehicle Model List
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
