3dd210290657058a4e0a1cbe8363ad40b00c9459
mark <yrt3ry@gmail.com>
2023-01-23 16:21:57 -0800

    inventory features

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import VehicleModelForm from './VehicleModelForm';
+import AutomobileForm from './AutomobileForm';
+import AutomobileList from './AutomobileList';
 
 function App() {
   return (
@@ -9,6 +12,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/automobiles/" element={<AutomobileList />} />
+          <Route path="/automobiles/new" element={<AutomobileForm />} />
+          <Route path="models/">
+
+						<Route path="new/" element={<VehicleModelForm />} />
+					</Route>
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -5,11 +5,27 @@ function Nav() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+						<NavLink className="dropdown-item" to="models/new">
+							Create a Vehicle Model
+						</NavLink>
+					</li>
+          <li className="nav-item">
+						<NavLink className="dropdown-item" to="automobiles/new">
+							Create an Automobile
+						</NavLink>
+					</li>
+          <li className="nav-item">
+            <NavLink className="dropdown-item" to="automobiles/">
+							List of Automobiles
+					  </NavLink>
+          </li>
           </ul>
         </div>
       </div>
