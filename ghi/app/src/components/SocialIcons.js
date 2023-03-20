28209d02889ad1810275c6d40ce849a051c78630
Andy Lam <andy.h.lam1@gmail.com>
2023-03-20 13:11:12 -0700

    social media icons

--- ghi/app/src/components/SocialIcons.js
+++ ghi/app/src/components/SocialIcons.js
@@ -10,17 +10,11 @@ const SocialIcons = () => {
 
   return (
     <div className="socialIcons" style={styles.socialIcons}>
-      <a className="icon" style={styles.icon} href="https://github.com/mdyeates">
-        <i className="fa-brands fa-github" aria-hidden="true" title="Michael Yeates' GitHub Profile"></i>
+      <a className="icon" style={styles.icon} href="https://gitlab.com/andyhlam619">
+        <i className="fa-brands fa-gitlab" aria-hidden="true" title="Michael Yeates' GitHub Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/mdyeates/">
+      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/andy-h-lam/">
         <i className="fa-brands fa-linkedin" aria-hidden="true" title="Michael Yeates' LinkedIn Profile"></i>
-      </a>
-      <a className="icon" style={styles.icon} href="https://www.instagram.com/michael.yeates/">
-        <i className="fa-brands fa-instagram" aria-hidden="true" title="Michael Yeates' Instagram Profile"></i>
-      </a>
-      <a className="icon" style={styles.icon} href="https://twitter.com/mdyeates">
-        <i className="fa-brands fa-twitter" aria-hidden="true" title="Michael Yeates' Twitter Profile"></i>
       </a>
     </div>
   );
